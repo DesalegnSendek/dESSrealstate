cd66379e9dde78c70e7304563397375460134d40
DesalegnSendek <desalegnsendek2@gmail.com>
2024-03-19 02:02:22 +0300

    additional pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
+import Home from "";
 import Profile from "./pages/Profile";
 import Offers from "./pages/Offers";
 import Signup from "./pages/Signup";
 import SignIn from "./pages/SignIn";
-
+import ForgotPassword from "./pages/ForgotPassword";
 
 function App() {
   return (
@@ -17,6 +17,7 @@ function App() {
           <Route path="/offers" element={<Offers />} />
           <Route path="/sign-up" element={<Signup />} />
           <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
         </Routes>
       </Router>
     </div>
