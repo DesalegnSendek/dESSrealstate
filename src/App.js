fd380dc1743db1a58873483cbc6ddc45f91ed9cb
DesalegnSendek <desalegnsendek2@gmail.com>
2024-03-19 01:54:21 +0300

    added pages and components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Profile from "./pages/Profile";
+import Offers from "./pages/Offers";
+import Signup from "./pages/Signup";
+import SignIn from "./pages/SignIn";
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/sign-up" element={<Signup />} />
+          <Route path="/sign-in" element={<SignIn />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
