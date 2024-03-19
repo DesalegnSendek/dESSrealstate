bdf18f81f58bcaf95a63f47116126270e72ac514
DesalegnSendek <desalegnsendek2@gmail.com>
2024-03-19 20:51:36 +0300

    index.js files for exporting files out from folders

---
+++ src/pages/index.jsx
@@ -0,0 +1,6 @@
+export { Offers } from './Offers';
+export { Home } from './Home';
+export { Profile } from './Profile';
+export { SignIn } from './SignIn';
+export { Signup } from './Signup';
+export { ForgotPassword } from './ForgotPassword';
